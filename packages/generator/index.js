bba8c6a82d49ac74c64c081423207b2822cf879c
Juani Galan <juan@lumapps.com>
2020-05-17 17:09:19 +0200

    log files

--- packages/generator/index.js
+++ packages/generator/index.js
@@ -1,8 +1,12 @@
 const core = require('@actions/core');
+const glob = require('@actions/glob');
 const packageJson = require('../../package.json');
 
 const main = async () => {
-  console.log(packageJson.workspaces);
+    const patterns = packageJson.workspaces;
+    const globber = await glob.create(patterns.join('\n'));
+    const files = await globber.glob();
+    console.log(files);
 }
 
 main().catch(err => core.setFailed(err.message));
