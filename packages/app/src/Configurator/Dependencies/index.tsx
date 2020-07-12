569af827da83bca3fb9e077c37b5f2ca40961309
Juani Galan <juan@lumapps.com>
2020-07-12 14:46:31 +0200

    refactor select

--- packages/app/src/Configurator/Dependencies/index.tsx
+++ packages/app/src/Configurator/Dependencies/index.tsx
@@ -1,61 +1,20 @@
 import React from 'react';
-import { List, ListItem, Select, Size } from '@lumx/react';
 import { useTranslate } from '../../Context';
-import { Label } from '../Label';
+import { Select } from '../Select';
 
 const OPTIONS = ['DEPENDENCIES', 'DEV_DEPENDENCIES', 'ALL_DEPENDENCIES'];
 
 const Dependencies: React.FC<any> = () => {
     const { translate } = useTranslate();
     const label = translate('CHOOSE_THE_DEPENDENCIES_YOU_WANT_TO_ANALYZE');
-    const helpers = [translate('CHOOSE_THE_DEPENDENCIES_YOU_WANT_TO_ANALYZE_HELP'), translate('TAKE_INTO_CONSIDERATION_THAT_SELECTING_ALL_DEPENDENCIES')];
-
-    const [value, setValue] = React.useState<string>(OPTIONS[0]);
-    const [isOpen, setIsOpen] = React.useState(false);
-
-    const onSelectOpen = () => {
-        setIsOpen(true);
-    }
-
-    const onSelectClose = () => {
-        setIsOpen(false);
-    }
-
-    const selectItem = (item: string) => () => {
-        if (value === item) {
-            setValue('');
-        } else {
-            setValue(item);
-        }
-
-        onSelectClose();
-    };
+    const helpers = [
+        translate('CHOOSE_THE_DEPENDENCIES_YOU_WANT_TO_ANALYZE_HELP'),
+        translate('TAKE_INTO_CONSIDERATION_THAT_SELECTING_ALL_DEPENDENCIES')
+    ];
 
     return (
-        <>
-            <Label label={label} helpers={helpers} />
-            <Select
-                isOpen={isOpen}
-                value={translate(value)}
-                onInputClick={onSelectOpen}
-                onDropdownClose={onSelectClose}
-            >
-                <List isClickable>
-                    {OPTIONS.map((choice, index) => (
-                            <ListItem
-                                isSelected={value === choice}
-                                key={index}
-                                onItemSelected={selectItem(choice)}
-                                size={Size.tiny}
-                            >
-                                {translate(choice)}
-                            </ListItem>
-                        ))
-                    }
-                </List>
-            </Select>
-        </>
-    );
+        <Select label={label} helpers={helpers} options={OPTIONS} />
+    )
 };
 
 export { Dependencies };
---
+++ packages/app/src/Configurator/Select/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { List, ListItem, Select as LumxSelect, Size } from '@lumx/react';
+import { useTranslate } from '../../Context';
+import { Label } from '../Label';
+
+export interface SelectProps {
+    label: string;
+    helpers?: string[];
+    options: string[];
+}
+
+const Select: React.FC<SelectProps> = ({ options, label, helpers }) => {
+    const { translate } = useTranslate();
+    const [value, setValue] = React.useState<string>(options[0]);
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const onSelectOpen = () => {
+        setIsOpen(true);
+    }
+
+    const onSelectClose = () => {
+        setIsOpen(false);
+    }
+
+    const selectItem = (item: string) => () => {
+        if (value === item) {
+            setValue('');
+        } else {
+            setValue(item);
+        }
+
+        onSelectClose();
+    };
+
+    return (
+        <>
+            <Label label={label} helpers={helpers} />
+            <LumxSelect
+                isOpen={isOpen}
+                value={translate(value)}
+                onInputClick={onSelectOpen}
+                onDropdownClose={onSelectClose}
+            >
+                <List isClickable>
+                    {options.map((choice, index) => (
+                            <ListItem
+                                isSelected={value === choice}
+                                key={index}
+                                onItemSelected={selectItem(choice)}
+                                size={Size.tiny}
+                            >
+                                {translate(choice)}
+                            </ListItem>
+                        ))
+                    }
+                </List>
+            </LumxSelect>
+        </>
+    );
+};
+
+export { Select };
