cdd35c12983e1381032f66905fca042625d001b9
Juani Galan <juan@lumapps.com>
2020-11-07 18:22:15 +0100

    ci check

--- packages/ga-check-dependencies/index.js
+++ packages/ga-check-dependencies/index.js
@@ -1,40 +1,49 @@
+const core = require('@actions/core');
 const { getWorkspaces, getPackagesFromWorkspaces } = require('@monorepolyser/dependencies');
 
 const repeatedDependencies = {};
 const deps = {};
 
 const main = async () => {
-    const workspaces = getWorkspaces()
+    try {
+        const workspaces = getWorkspaces()
 
-    const { packages } = await getPackagesFromWorkspaces(workspaces);
+        const { packages } = await getPackagesFromWorkspaces(workspaces);
 
-    Object.keys(packages).forEach((pkgName) => {
-        const pkg = packages[pkgName];
-        const { dependencies } = pkg;
+        Object.keys(packages).forEach((pkgName) => {
+            const pkg = packages[pkgName];
+            const { dependencies } = pkg;
 
-        Object.keys(dependencies).forEach((dep) => {
-            const version = dependencies[dep];
+            Object.keys(dependencies).forEach((dep) => {
+                const version = dependencies[dep];
 
-            if (deps[dep]) {
-                const detectedVersion = deps[dep];
+                if (deps[dep]) {
+                    const detectedVersion = deps[dep];
 
-                if (version !== detectedVersion) {
-                    if (!repeatedDependencies[dep]) {
-                        repeatedDependencies[dep] = [];
+                    if (version !== detectedVersion) {
+                        if (!repeatedDependencies[dep]) {
+                            repeatedDependencies[dep] = [];
+                        }
+
+                        repeatedDependencies[dep].push({
+                            addedBy: pkgName,
+                            version, 
+                        });
                     }
-
-                    repeatedDependencies[dep].push({
-                        addedBy: pkgName,
-                        version, 
-                    });
+                } else {
+                    deps[dep] = version;
                 }
-            } else {
-                deps[dep] = version;
-            }
+            });
         });
-    });
 
-    console.log(repeatedDependencies);
+        if (Object.keys(repeatedDependencies).length > 0) {
+            throw new Error({
+                message: 'there are deps with different versions'
+            });
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 main();
